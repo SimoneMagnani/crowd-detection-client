16d828151db86c25bcfea4005e5324444a0f5280
Simone <Simone.magnani4@studio.unibo.it>
2021-05-19 19:02:24 +0200

    remove useless logs

--- client/src/app/helper/interceptor/jwt.interceptor.ts
+++ client/src/app/helper/interceptor/jwt.interceptor.ts
@@ -16,14 +16,11 @@ export class JWTInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     // add auth header with jwt if user is logged in and request is to the api url
     const user = this.accountService.userValue;
-    console.log(user)
     if (user === null) {
       return next.handle(request);
     }
     const isLoggedIn = user && user.access_token;
-    console.log(isLoggedIn)
     const isApiUrl = request.url.startsWith(this.apiURL.baseApiUrl);
-    console.log(isApiUrl)
     if (isLoggedIn && isApiUrl) {
       request = request.clone({
         setHeaders: {
