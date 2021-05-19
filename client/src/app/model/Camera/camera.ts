21e4041535a7e004bc715adeeea1e6068c6d5671
Simone <Simone.magnani4@studio.unibo.it>
2021-05-19 16:06:06 +0200

    add camera model

---
+++ client/src/app/model/Camera/camera.ts
@@ -0,0 +1,8 @@
+export interface Camera {
+  "camera_id": String,
+  "camera_name": String,
+  "camera_address": String,
+  "topic_root": String,
+  "mqtt_broker_ip": String ,
+  "mqtt_broker_port": number
+}
