5d682cc13eff8e9f6bb2e0830233e432860c241a
Simone <Simone.magnani4@studio.unibo.it>
2021-05-21 17:58:45 +0200

    camname not required

--- client/src/app/model/Camera/camera.ts
+++ client/src/app/model/Camera/camera.ts
@@ -1,6 +1,6 @@
 export interface Camera {
   "camera_id": string,
-  "camera_name": string,
+  "camera_name"?: string,
   "camera_address": string,
   "topic_root": string,
   "mqtt_broker_ip": string ,
